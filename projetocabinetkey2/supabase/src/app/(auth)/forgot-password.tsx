98c99686024073d4759fdf62ad12432ff1f3fdd8
João Vitor <jo4ovitor4004@gmail.com>
2025-05-08 21:53:28 -0300

    08/05/2025

--- projetocabinetkey2/supabase/src/app/(auth)/forgot-password.tsx
+++
@@ -1,8 +0,0 @@
-interface CustomHref extends Record<string, string> {
-    '/': string;
-    '/(auth)/signin/page': string;
-    '/(auth)/signup/page': string;
-    '/(auth)/forgot-password': string;
-    '/(panel)/profile/page': string;
-    // Adicione outras rotas conforme necessário
-  }
